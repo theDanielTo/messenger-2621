46e7097008c75941f2d3eda098f32643972a9ab3
theDanielTo <dto1989@gmail.com>
2021-11-24 12:17:05 -0800

    Sorted messages by 'id' inside of Messages component

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const [sortedMessages, setSortedMessages] = useState([]);
+
+  useEffect(() => {
+    setSortedMessages(() => {
+      return messages.sort((a, b) => {
+        return (a.id < b.id) ? -1 : 1;
+      });
+    });
+  }, []);
+
 
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
-        console.log(message.id, message)
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
+      {sortedMessages.map((message) => {
+          const time = moment(message.createdAt).format("h:mm");
+
+          return message.senderId === userId ? (
+            <SenderBubble key={message.id} text={message.text} time={time} />
+          ) : (
+            <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          );
       })}
     </Box>
   );
